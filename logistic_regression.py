d183adece9797ed1f1c112df18f5b83fc404b104
Edward Junsyoung Lee <junsyounged.lee@mail.utoronto.ca>
2025-11-06 13:00:49 -0500

    validation with learning rate .25 val ce = 0.052

--- logistic_regression.py
+++ logistic_regression.py
@@ -129,7 +129,7 @@ def run_logistic_regression():
     # of iterations                                                     #
     #####################################################################
     hyperparameters = {
-        "learning_rate": 0.1,
+        "learning_rate": 0.25,
         "weight_regularization": 0.,
         "num_iterations": 2000
     }
